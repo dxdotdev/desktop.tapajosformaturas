3986560efe8448aabbd059ded8b417897e135730
Davi Reis <dreisss.dev@gmail.com>
2025-01-20 22:46:52 -0300

    feat: adjusts to build to windows

--- shell.nix
+++ shell.nix
@@ -2,20 +2,17 @@ let
   pkgs = import <nixpkgs> { };
 in
 pkgs.mkShell {
-  shellHook = ''
-    pnpm i
-    pnpm tauri dev
-  '';
-
   nativeBuildInputs = with pkgs; [
     pkg-config
     gobject-introspection
+    rustup
     cargo
     cargo-tauri
+    cargo-xwin
     nodejs
   ];
 
-  buildInputs = with pkgs;[
+  buildInputs = with pkgs; [
     at-spi2-atk
     atkmm
     cairo
@@ -28,5 +25,12 @@ pkgs.mkShell {
     pango
     webkitgtk_4_1
     openssl
+    nsis
+    lld_18
+    libllvm
+    libclang
   ];
+
+  # fixes to build windows target in linux: https://v2.tauri.app/distribute/windows-installer/
+  # rustup target add x86_64-pc-windows-msvc
 }
