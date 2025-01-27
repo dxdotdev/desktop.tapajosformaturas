f07ae389d9748ea419dfbf02c410f36565ba0e62
Davi Reis <dreisss.dev@gmail.com>
2025-01-27 07:52:21 -0300

    revert: "feat!: ui restart"
    This reverts commit 9c977d7e1c1aa4c8b5c3d6a772ed48ab5f951049.

--- src/components/blocks/header.tsx
+++ src/components/blocks/header.tsx
@@ -1,25 +1,27 @@
 import { getCurrentWindow } from '@tauri-apps/api/window'
-import { Minus, X } from 'lucide-react'
+import { Minus, Square, X } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 
 export function Header() {
   const window = getCurrentWindow()
 
-  function handleMinimize() {
-    window.minimize()
-  }
-
-  function handleClose() {
-    window.close()
-  }
-
   return (
-    <header className="absolute flex w-full justify-end" data-tauri-drag-region>
-      <Button variant="ghost" size="icon" className="w-12 rounded-none" onClick={handleMinimize}>
+    <header className="flex justify-end" data-tauri-drag-region>
+      <Button variant="ghost" size="icon" className="w-12 rounded-none" onClick={() => window.minimize()}>
         <Minus />
       </Button>
-      <Button variant="ghost" size="icon" className="w-12 rounded-none hover:bg-destructive" onClick={handleClose}>
+
+      <Button variant="ghost" size="icon" className="w-12 rounded-none" onClick={() => window.toggleMaximize()}>
+        <Square className="scale-75" />
+      </Button>
+
+      <Button
+        variant="ghost"
+        size="icon"
+        className="w-12 rounded-none hover:bg-destructive"
+        onClick={() => window.close()}
+      >
         <X />
       </Button>
     </header>
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,3 @@
-import '@/globals.css'
-import '@fontsource/geist-sans/400.css'
-import '@fontsource/geist-sans/500.css'
-import '@fontsource/geist-sans/700.css'
-
 import React from 'react'
 // biome-ignore lint/style/useNamingConvention:
 import ReactDOM from 'react-dom/client'
