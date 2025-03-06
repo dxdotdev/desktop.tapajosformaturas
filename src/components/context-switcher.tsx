14cf9af354d0d6b03e4bb00c1a97c07309c473d8
Davi Reis <dreisss.dev@gmail.com>
2025-03-06 08:13:15 -0300

    feat: add better context types

--- src/components/context-switcher.tsx
+++ src/components/context-switcher.tsx
@@ -47,6 +47,7 @@ export function ContextSwitcher() {
               <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
                 <currentContext.icon className="size-4" />
               </div>
+
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">{contextName}</span>
                 <span className="truncate text-xs">{currentContext.course}</span>
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,12 +2,28 @@ import type { LucideIcon } from 'lucide-react'
 
 import { SIDEBAR_NAV_CONTENT } from '@/lib/constants'
 
-const labels = SIDEBAR_NAV_CONTENT.pages.map((p) => p.label)
-export type Page = (typeof labels)[number]
+export type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+
+export type Contract = `${Digit}${Digit}${Digit}${Digit}`
+
+export type Institution = 'UNAMA' | 'IESPES' | 'CEPES' | 'UNIP' | 'UFOPA' | 'UEPA' | 'IFPA'
+
+export type Course =
+  | 'Medicina'
+  | 'Enfermagem'
+  | 'Técnico em Enfermagem'
+  | 'Pedagogia'
+  | 'Psicologia'
+  | 'Farmácia'
+  | 'Direito'
+  | 'Engenharia Civil'
 
 export type Context = {
-  institution: string
-  contractNumber: string
-  course: string
+  institution: Institution
+  contract: Contract
+  course: Course
   icon: LucideIcon
 }
+
+const labels = SIDEBAR_NAV_CONTENT.pages.map((p) => p.label)
+export type Page = (typeof labels)[number]
